d556a3d4330da2b51ede6c3621eac3c138a396b7
avil13 <13.avil@gmail.com>
2016-01-06 20:01:47 +0300

    line reindent

--- src/index.js
+++ src/index.js
@@ -6,36 +6,36 @@ var ext = require('gulp-util').replaceExtension;
 var PluginError = require('gulp-util').PluginError;
 
 function handleCompile(contents, opts) {
-    return compile(contents, opts)(opts.locals || opts.data);
+  return compile(contents, opts)(opts.locals || opts.data);
 }
 
 module.exports = function(options) {
-    var opts = options || {},
-        extension = opts.extension !== undefined ? opts.extension : '.html';
+  var opts = options || {},
+  extension = opts.extension !== undefined ? opts.extension : '.html';
 
-    function CompileSlm(file, enc, cb) {
-        opts.filename = file.path;
+  function CompileSlm(file, enc, cb) {
+    opts.filename = file.path;
 
-        if (file.data) {
-            opts.data = file.data;
-        }
-
-        file.path = ext(file.path, extension);
-
-        if (file.isStream()) {
-            return cb(new PluginError('gulp-slm', 'Streaming not supported'));
-        }
-
-        if (file.isBuffer()) {
-            try {
-                file.contents = new Buffer(handleCompile(String(file.contents), opts));
-            } catch (e) {
-                return cb(new PluginError('gulp-slm', e));
-            }
-        }
-
-        cb(null, file);
+    if (file.data) {
+      opts.data = file.data;
     }
 
-    return through.obj(CompileSlm);
+    file.path = ext(file.path, extension);
+
+    if (file.isStream()) {
+      return cb(new PluginError('gulp-slm', 'Streaming not supported'));
+    }
+
+    if (file.isBuffer()) {
+      try {
+        file.contents = new Buffer(handleCompile(String(file.contents), opts));
+      } catch (e) {
+        return cb(new PluginError('gulp-slm', e));
+      }
+    }
+
+    cb(null, file);
+  }
+
+  return through.obj(CompileSlm);
 };
